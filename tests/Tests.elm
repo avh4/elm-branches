c961ebeb9f0963c26d13921dc7275df67051072e
Aaron VonderHaar <gruen0aermel@gmail.com>
2016-03-08 11:42:13 -0800

    elm-format all files

--- tests/Tests.elm
+++ tests/Tests.elm
@@ -1,15 +1,14 @@
-module Tests where
+module Tests (..) where
 
 import ElmTest exposing (..)
-
 import String
 
 
 all : Test
 all =
-    suite "A Test Suite"
-        [
-            test "Addition" (assertEqual (3 + 7) 10),
-            test "String.left" (assertEqual "a" (String.left 1 "abcdefg")),
-            test "This test should fail" (assert False)
-        ]
+  suite
+    "A Test Suite"
+    [ test "Addition" (assertEqual (3 + 7) 10)
+    , test "String.left" (assertEqual "a" (String.left 1 "abcdefg"))
+    , test "This test should fail" (assert False)
+    ]
